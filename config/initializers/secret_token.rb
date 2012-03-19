9dd02d0515c8ab267c854a73a2f6e0eb5a5283f3
Kenny Meyer <knny.myer@gmail.com>
2012-03-19 20:35:39 -0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FindMyMoney::Application.config.secret_token = '0cd838a1c8eefaef917bced9fdb817f0251c30dcbd8ed08bfbc324c5b58ac4b327f851b731432c3961ec6d4903793d64d556f4cb7440f4ef1f685778970bf7f9'

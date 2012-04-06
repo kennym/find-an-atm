dd0b752c5fa62c94c833143a915dc3d2e8013053
Kenny Meyer <knny.myer@gmail.com>
2012-04-06 17:58:30 -0300

    Modify migration...

--- db/migrate/20120319233516_create_nodes.rb
+++ db/migrate/20120319233516_create_nodes.rb
@@ -3,8 +3,8 @@ class CreateNodes < ActiveRecord::Migration
     create_table :nodes do |t|
       t.string "name"
       t.string "address"
-      t.float "latitude"
-      t.float "longitude"
+      t.float "latitude", :limit => 55
+      t.float "longitude", :limit => 55
       t.integer "node_type_id"
 
       t.timestamps
--- db/migrate/20120320000200_create_node_types.rb
+++
@@ -1,9 +0,0 @@
-class CreateNodeTypes < ActiveRecord::Migration
-  def change
-    create_table :node_types do |t|
-      t.string :name
-
-      t.timestamps
-    end
-  end
-end

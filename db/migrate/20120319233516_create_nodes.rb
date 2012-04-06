c8efcbd26ebb6b01c4b4b1c9a57e2d008d2bc9de
Kenny Meyer <knny.myer@gmail.com>
2012-04-06 18:01:40 -0300

    Bla

--- db/migrate/20120319233516_create_nodes.rb
+++ db/migrate/20120319233516_create_nodes.rb
@@ -3,8 +3,8 @@ class CreateNodes < ActiveRecord::Migration
     create_table :nodes do |t|
       t.string "name"
       t.string "address"
-      t.float "latitude", :limit => 55
-      t.float "longitude", :limit => 55
+      t.float "latitude", :scale => 12, :precision => 15
+      t.float "longitude", :scale => 12, :precision => 15
       t.integer "node_type_id"
 
       t.timestamps

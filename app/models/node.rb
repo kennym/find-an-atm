a34bc78730b299700acaa8f3fd637faf2dbec021
Kenny Meyer <knny.myer@gmail.com>
2012-04-07 21:59:34 -0300

    Add validations

--- app/models/node.rb
+++ app/models/node.rb
@@ -2,5 +2,8 @@ class Node < ActiveRecord::Base
   attr_accessible :address, :latitude, :longitude, :name
   reverse_geocoded_by :latitude, :longitude, :address => :location
 
+  validates :name, :latitude, :longitude, :presence => true
+  validates :name, :length => { :maximum => 35 }
+
   after_validation :reverse_geocode
 end
